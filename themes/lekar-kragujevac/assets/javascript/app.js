255afebecbf1bad3ec7a59ac51b73569c95cb89e
stevan <andric.stevan@yhahoo.com>
2018-10-25 22:06:53 +0200

    Finalizacija inicijalne strukture

--- themes/lekar-kragujevac/assets/javascript/app.js
+++ themes/lekar-kragujevac/assets/javascript/app.js
@@ -1,2 +1,56 @@
-$(document).ready(function($){
-});
+/**
+ * Application's main entry point.
+ *
+ * LICENSE: This code is licenced to Andrić Stevan
+ *
+ * @package    Projekti
+ * @copyright  2018 Andrić Stevan
+ * @author     Stevan Andrić <stevan@telekod.com>
+ * @version    $Id: app.js 2667 2018-04-26 15:44:49Z stevan $
+ * @since      File available since Release 0
+ */
+App = {
+  
+  /**
+   * Is debug, used when logging with the console.
+   */
+  // debug: false,
+
+  /**
+   * Is mobile device.
+   */
+  // isMobile: false,
+
+  /**
+   * Is tablet device.
+   */
+  // isTablet: false,
+
+  /**
+   * IE version.
+   */
+  // ieVersion: '',
+
+  /**
+   * Site URL.
+   */
+  // siteUrl: '',
+
+  /**
+   * Static content unix timestamp.
+   */
+  // staticContentTs: '1501069777',
+
+  /**
+   * Initializing App.
+   */
+  _init: function () {
+
+    $(document).ready(function() {
+      if (App.debug) {
+        console.log("[App] Application's main entry point.");
+      }
+    });
+    
+  }
+};
---
+++ themes/lekar-kragujevac/layouts/defaultOnEnd.php
@@ -0,0 +1,38 @@
+﻿<?php
+////////////////////////////////////////////////////////////////////////////////
+// CSS
+////////////////////////////////////////////////////////////////////////////////
+// Default
+$cssFiles = [
+    'assets/vendor/owl-carousel/css/owl.css',
+    'assets/vendor/foundation/foundation.min.css',
+    '@framework.extras',
+    'assets/less/theme.less'
+];
+// Mobile CSS
+// if ($this['isMobile']) {
+//    $cssFiles[] = 'assets/css/mobile.css';
+// }
+$this['cssFiles'] = $cssFiles;
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Default JavaScript
+////////////////////////////////////////////////////////////////////////////////
+// Body
+$jsBodyFiles = [
+    'assets/vendor/owl-carousel/javascript/owl.carousel.min.js',
+    '@framework',
+    '@framework.extras',
+    'assets/javascript/app.js',
+];
+
+// Add JavaScript body files defined in plugin(s)
+if (isset($this['jsBodyFilesPlugins']) && !empty($this['jsBodyFilesPlugins'])) {
+    foreach($this['jsBodyFilesPlugins'] as $jsBodyFilesPlugins) {
+        $jsBodyFiles[] = $jsBodyFilesPlugins;
+    }
+}
+
+$this['jsBodyFiles'] = $jsBodyFiles;
+////////////////////////////////////////////////////////////////////////////////
